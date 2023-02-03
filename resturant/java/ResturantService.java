665e15d5ae562532a0d26bf40e08c07aeacd757c
Mangaldip Ghosh <87035507+ghoshmangaldip124@users.noreply.github.com>
2023-02-03 23:07:58 +0530

    Update ResturantService.java

--- resturant/java/ResturantService.java
+++ resturant/java/ResturantService.java
@@ -1,12 +1,18 @@
+// @ Author :- Mangaldip Ghosh
 package resturant.java;
 
 public class ResturantService {
 
 	public static void main(String[] args) {
+		// object for ResturantMain Class
 		ResturantMain resturantMain = new ResturantMain();
+		//Array for Create menu Method
 		resturantMain.CreateMenu();
+		//Menu print Method
 		ResturantMain.getDisplay();
+		//Menu order Method
 		ResturantMain.getOrder();
+		//Menu for order and billing Method
 		ResturantMain.getBilling();
 	}
 
